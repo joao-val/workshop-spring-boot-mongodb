fb6d2e462a320baf5870f56ab95b0402181872c3
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2021-09-28 01:25:02 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/joaoval/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.joaoval.workshopmongo.repository;
+
+import com.joaoval.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
