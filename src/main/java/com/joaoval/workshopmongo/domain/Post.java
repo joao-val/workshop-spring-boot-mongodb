be47c89094e801a5c24157f77897afe78bf5b5ae
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2021-09-28 22:55:00 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/joaoval/workshopmongo/domain/Post.java
+++ src/main/java/com/joaoval/workshopmongo/domain/Post.java
@@ -1,12 +1,12 @@
 package com.joaoval.workshopmongo.domain;
 
 import com.joaoval.workshopmongo.dto.AuthorDTO;
+import com.joaoval.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.Objects;
+import java.util.*;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +18,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -68,6 +70,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
